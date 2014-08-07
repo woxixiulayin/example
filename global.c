052f3c6d3e19a7f498f72a93637cacb5c3407ad9
gang <416883813@qq.com>
2014-08-07 23:41:48 +0800

    加入box 和squar函数

--- global.c
+++ global.c
@@ -2,50 +2,106 @@
 #include "global.h"
 
 void draw_windows(void)
-{
+    {
     glClear(GL_COLOR_BUFFER_BIT);
+
+     square_with_board(0, 0, 1.0, windows_line_width, blue, black);
+
+
+    glFlush();
+    }
+
+void square_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width, color board[], color inside[])
+    {
+    glColor3ubv(board);
+    glBegin(GL_QUADS);
+
+    glVertex2f(x - f_size , y + f_size);
+    glVertex2f(x + f_size , y + f_size);
+    glVertex2f(x + f_size , y - f_size);
+    glVertex2f(x - f_size , y - f_size);
+
+    glEnd();
+
+    glColor3ubv(inside);
+    glBegin(GL_QUADS);
+
+    glVertex2f(x - f_size + width, y + f_size - width);
+    glVertex2f(x + f_size - width, y + f_size - width);
+    glVertex2f(x + f_size - width, y - f_size + width);
+    glVertex2f(x - f_size + width, y - f_size + width);
+
+    glEnd();
+
+    }
+
+void draw_box(int x, int y)
+{
+  //  square_with_board(x , y, box_size * 0.8, box_size * 0.1,  black,  white );
+    int f_size = box_size * 4 / 5;
+    int width = box_size / 10;
+    glColor3ubv(black);
+    glBegin(GL_QUADS);
+
+    glVertex2i(x - f_size , y + f_size);
+    glVertex2i(x + f_size  , y + f_size);
+    glVertex2i(x + f_size  , y - f_size);
+    glVertex2i(x - f_size , y - f_size);
+
+    glEnd();
 
     glColor3ubv(white);
     glBegin(GL_QUADS);
 
-    glVertex2f(-1.0f, 1.0f);
-    glVertex2f(1.0f, 1.0f);
-    glVertex2f(1.0f, -1.0f);
-    glVertex2f(-1.0f, -1.0f);
+    glVertex2i(x - (f_size - width) , y + f_size - width);
+    glVertex2i(x + (f_size - width) , y + f_size - width);
+    glVertex2i(x + (f_size - width) , y - f_size + width);
+    glVertex2i(x - (f_size - width) , y - f_size + width);
 
     glEnd();
 
+}
 
-    glColor3ubv(black);
-
-    glBegin(GL_QUADS);
-
-    glVertex2f(-1.0f + windows_line_width, 1.0f- windows_line_width);
-    glVertex2f(1.0f- windows_line_width, 1.0f- windows_line_width);
-    glVertex2f(1.0f- windows_line_width, -1.0f+ windows_line_width);
-    glVertex2f(-1.0f+ windows_line_width, -1.0f+ windows_line_width);
-    glEnd();
-
-    glFlush();
+void draw_board(void)
+{
+    int i;
+    //GLfloat x = board_left_position;
+    GLfloat y = -1.0 + 0.5 * box_size;
+    for(i = 0; i < 17 ; i++)
+    {
+        draw_box(board_left_position, y );
+        draw_box(board_right_position, y );
+        y += box_size;
+    }
 }
 
 void mydisplay(void)
-{
-    draw_windows();
+    {
+    glClear(GL_COLOR_BUFFER_BIT);
+  //  draw_windows();
+ //   square_with_board(0,0,0.5,0.02,blue,red);
+ //   draw_box(0, 0);
+    //draw_board();
+     glColor3ubv(white);
+ glBegin(GL_LINES);
+glVertex2i(100, 200);
+glVertex2i(1,0);
+glEnd();
     glFlush();
-}
+    }
 
 int main(int argc, char *argv[])
-{
+    {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
 
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(150, 70);
     glutInitWindowSize(windows_width, windows_height);
     glutCreateWindow("第一个绘图窗口");
 
     glutDisplayFunc(&mydisplay);
+
     glutMainLoop();
     return 0;
-}
+    }
 
--- global.h
+++ global.h
@@ -3,33 +3,37 @@
 
 #include <GL/glut.h>
 
-
+typedef  GLubyte color;
 //设置颜色
-const GLubyte green[3] = {0,255,0};
-const GLubyte red[3]= {255, 0, 0};
-const GLubyte blue[3]= {0, 0, 255};
-const GLubyte black[3] = {0, 0, 0};
-const GLubyte white[3] = {255, 255, 255};
+const color green[3] = {0,255,0};
+const color red[3]= {255, 0, 0};
+const color blue[3]= {0, 0, 255};
+const color black[3] = {0, 0, 0};
+const color white[3] = {255, 255, 255};
 
 //定义界面窗口的大小 单位为像素点
 #define windows_width 1000
-#define windows_height 800 
+#define windows_height 600
+#define windows_scale (1.0*windows_height/windows_width)
 
 //定义游戏区的大小 单位为方块
 #define board_width 10
 #define board_height 15
 
 //定义窗口边界线的粗细 单位为像素比例
-//#define windows_line_width ( 2.0f / (board_height + 10.0f) )
-#define windows_line_width 0.04f
+#define windows_line_width ( 2.0f / (board_height + 2))
+//#define windows_line_width 0.04f
 
-//定义游戏区在界面中的位置，用距离左边界的距离表示  单位为窗口的比例
-#define board_left_position 0.8
+//定义小方块的大小  单位为像素点 游戏区高度为15个box 在加上边界线
+#define box_size (windows_height /( board_height+2))
+//定义游戏区在界面中的位置，用距离左边界的距离表示  单位为像素点
+#define board_left_position  -0.6
+#define board_right_position (11 *  box_size +  board_left_position)
 
 
 //定义预览窗口在界面中的位置,用中心点位置表示  单位为窗口比例位置
 #define preview_x_positon 0.5
-#define preview_y_positon 0.5 
+#define preview_y_positon 0.5
 
 //定义预览窗口的大小 单位为窗口的比例位置
 #define preview_height  0.5
@@ -37,9 +41,8 @@ const GLubyte white[3] = {255, 255, 255};
 
 //定义预览窗口线的粗细 单位为窗口的比例
 #define preview_line_width   0.02
- 
-//定义小方块的大小  单位为窗口比例 游戏区高度为15个box 在加上边界线
-#define box_size (2.0f / (board_height + 2.0f))
+
+
 
 
 //画界面边线和背景
@@ -51,4 +54,10 @@ void draw_board(void);
 //画预览区
 void draw_preview(void);
 
-#endif 
+//带边界的矩形
+void square_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width, color board[], color inside[]);
+
+//画小方块 保证在4：3的窗口下是正方形
+void draw_box(int x, int y);
+
+#endif
