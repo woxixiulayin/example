a1035a1f6c446ad75e63c386d218d6c536cdbdc5
gang <416883813@qq.com>
2014-08-10 22:46:50 +0800

    添加方块数组和预览方块的生成函数

--- global.c
+++ global.c
@@ -1,92 +1,112 @@
 #include <stdio.h>
+#include <math.h>
 #include "global.h"
 
 void draw_windows(void)
     {
     glClear(GL_COLOR_BUFFER_BIT);
 
-     square_with_board(0, 0, 1.0, windows_line_width, white, black);
-
-
-    glFlush();
+    printf("call function %s!  \n", __func__);
+     rect_with_board(0, 0, 2.0, windows_line_width, rect, white, black);
+     glFlush();
     }
 
-void square_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width, color board[], color inside[])
+void rect_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width, int squar_flag, color board[], color inside[])
     {
+
+    float scale = 1.0;
+    float _size = f_size / 2.0;
+    if(squar_flag == squar) scale = windows_scale;
+
+   // printf("call function %s!  \n", __func__);
+
     glColor3ubv(board);
     glBegin(GL_QUADS);
 
-    glVertex2f(x - f_size , y + f_size);
-    glVertex2f(x + f_size , y + f_size);
-    glVertex2f(x + f_size , y - f_size);
-    glVertex2f(x - f_size , y - f_size);
+    glVertex2f(x - _size * scale , y + _size);
+    glVertex2f(x + _size * scale , y + _size);
+    glVertex2f(x + _size * scale , y - _size);
+    glVertex2f(x - _size * scale , y - _size);
 
     glEnd();
 
     glColor3ubv(inside);
     glBegin(GL_QUADS);
 
-    glVertex2f(x - f_size + width, y + f_size - width);
-    glVertex2f(x + f_size - width, y + f_size - width);
-    glVertex2f(x + f_size - width, y - f_size + width);
-    glVertex2f(x - f_size + width, y - f_size + width);
+    glVertex2f(x - (_size - width) * scale, y + _size - width);
+    glVertex2f(x + (_size - width) * scale, y + _size - width);
+    glVertex2f(x + (_size - width) * scale, y - _size + width);
+    glVertex2f(x - (_size - width) * scale, y - _size + width);
 
     glEnd();
 
     }
 
-void draw_box(GLfloat x, GLfloat y)
+void draw_box(GLfloat x, GLfloat y, color b_color[])
 {
-  //  square_with_board(x , y, box_size * 0.8, box_size * 0.1,  black,  white );
-    //注意此处的长和宽应该是除以2之后的，以中心坐标来计算4个角的坐标,且边界与大小的比为1/10
-    GLfloat f_size_x = box_size_x / 2;
-    GLfloat f_size_y = box_size_y / 2;
-    GLfloat width_x = box_size_x * box_scale;
-    GLfloat width_y = box_size_y * box_scale;
-
-    glColor3ubv(black);
-    glBegin(GL_QUADS);
-
-    glVertex2f(x - f_size_x, y + f_size_y);
-    glVertex2f(x + f_size_x , y + f_size_y);
-    glVertex2f(x + f_size_x  , y - f_size_y);
-    glVertex2f(x - f_size_x , y - f_size_y);
-
-    glEnd();
-
-    glColor3ubv(white);
-    glBegin(GL_QUADS);
-
-    glVertex2f(x - (f_size_x - width_x) , y + f_size_y - width_y);
-    glVertex2f(x + (f_size_x - width_x) , y + f_size_y - width_y);
-    glVertex2f(x + (f_size_x - width_x) , y - f_size_y + width_y);
-    glVertex2f(x - (f_size_x - width_x) , y - f_size_y + width_y);
-
-    glEnd();
-
+    rect_with_board(x, y, box_size , box_size * box_scale , 1,  black, b_color);
 }
 
+int rand_shape(void)
+{
+	return rand()%shape_max;
+}
+
+void draw_pre_shape(int shape_num)
+{
+	GLfloat X = 0.45;
+	GLfloat Y = 0.62;
+	GLfloat x = X;
+	GLfloat y = Y;
+	//先清掉上次的图像
+	draw_preview();
+
+    	printf("call function %s! shape num is %d  \n", __func__, shape_num);
+
+        int i = 0;
+	int j = 0;
+	char flag = 0;
+	char mask = 0x80;
+	for(i = 0; i<2; i++)
+		{
+		flag = shapes[shape_num].box[i];
+		if(i == 1) y -= box_size;
+		for(j =0; j<8; j++)
+			{
+				if(4 == j)  y -= box_size;
+				x = X + box_size_x * (j%4);
+				if(flag& mask)
+                    draw_box(x, y, shapes[shape_num].color );
+                flag <<= 1;
+			}
+		}
+}
 void draw_board(void)
 {
     int i;
-    //GLfloat x = board_left_position;
-    printf("begin function %s!  \n", __func__);
-    GLfloat y = -1.0 + 0.5 * box_size_y;
-    for(i = 0; i < 17 ; i++)
+    printf("call function %s!  \n", __func__);
+    GLfloat y = -1.0 + 1.5 * box_size;
+    for(i = 0; i < 15 ; i++)
     {
-        draw_box(board_left_position, y );
-        draw_box(board_right_position, y );
-        y += box_size_y;
+        draw_box(board_left_position, y, blue);
+        draw_box(board_right_position, y, blue);
+        y += box_size;
     }
 }
 
+void draw_preview(void)
+{
+	rect_with_board(preview_x, preview_y, preview_size, preview_line_width, squar,red, black);
+}
 void mydisplay(void)
     {
     glClear(GL_COLOR_BUFFER_BIT);
     draw_windows();
+    draw_preview();
     //square_with_board(0,0,0.5,0.02,blue,red);
-    draw_box(0, 0);
+  //  draw_box(0.6, 0.4, orange);
     draw_board();
+   draw_pre_shape(rand_shape());
     glFlush();
     }
 
@@ -95,11 +115,14 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
 
+
     glutInitWindowPosition(150, 70);
     glutInitWindowSize(windows_width, windows_height);
     glutCreateWindow("第一个绘图窗口");
 
     glutDisplayFunc(&mydisplay);
+
+
 
     glutMainLoop();
     return 0;
--- global.h
+++ global.h
@@ -3,13 +3,6 @@
 
 #include <GL/glut.h>
 
-typedef  GLubyte color;
-//设置颜色
-const color green[3] = {0,255,0};
-const color red[3]= {255, 0, 0};
-const color blue[3]= {0, 0, 255};
-const color black[3] = {0, 0, 0};
-const color white[3] = {255, 255, 255};
 
 //定义界面窗口的大小 单位为像素点
 #define windows_width 1000
@@ -25,28 +18,160 @@ const color white[3] = {255, 255, 255};
 //#define windows_line_width 0.04f
 
 //定义小方块的大小,边界与大小的比值  单位为比例值 游戏区高度为15个box 在加上边界线
-#define box_size_y (2.0 / ( board_height+2))
-#define box_size_x (2.0 / ( board_height+2) * windows_height/ windows_width) 
+#define box_size (2.0 / ( board_height+2))
 #define box_scale (1.0/18)
-
+#define box_size_y  box_size
+#define box_size_x box_size*windows_scale
 //定义游戏区在界面中的位置，用距离左边界的距离表示  单位为像素点
-#define board_left_position  -0.4
-#define board_right_position (11 *  box_size_x +  board_left_position)
+#define board_left_position  -0.6
+#define board_right_position (11 * box_size * windows_height / windows_width +  board_left_position)
 
 
-//定义预览窗口在界面中的位置,用中心点位置表示  单位为窗口比例位置
-#define preview_x_positon 0.5
-#define preview_y_positon 0.5
-
-//定义预览窗口的大小 单位为窗口的比例位置
-#define preview_height  0.5
-#define preview_width   0.5
+//定义预览窗口的大小和位置  单位为窗口的比例位置
+#define preview_size  0.6
+#define preview_x   0.54
+#define preview_y   0.45
 
 //定义预览窗口线的粗细 单位为窗口的比例
-#define preview_line_width   0.02
+#define preview_line_width   0.01
+
+//定义正方形和矩形的宏变量值
+#define squar 1
+#define rect 0
+
+//定义形状的数量
+#define shape_max 19
+
+typedef  GLubyte color;
+//设置颜色
+const color green[3] = {0,255,0};
+const color red[3]= {255, 0, 0};
+const color blue[3]= {0, 0, 255};
+const color black[3] = {0, 0, 0};
+const color white[3] = {0xff,0xff,0xfb};
+const color orange[3] = {0xf1, 0x5a, 0x22};
+const color yellow[3] = {0xff, 0xd4, 0x00};
+const color purple[3] = {0x85, 0x52, 0xa1};
+const color brown[3] = {0x53, 0x26, 0x1f};
+
+
+//定义游戏区底板
+struct board{
+	int val;  //0表示不显示  1表示显示
+	color col;  //表示该点的颜色
+}table_boad[board_width][board_height];
+
+//定义方块的结构体
+typedef struct {
+	char box[2];
+	int color;//方块的颜色
+	int next; //该放快下一个形状所在的索引号
+}shape;
+
+const shape shapes[shape_max] = {
+
+	/*   口口
+       口口   x0.5  y0*/
+	{0x6c,0x0, yellow,1},
+/*   口
+       口口
+           口    */
+
+	{0x8c,0x40, yellow, 0},
+	/*  口口
+	          口口     */
+	{0xc6,0x0, blue, 3},
+
+	/*  口
+	  口口
+	  口   */
+	{0x4c,0x80, blue, 2},
+
+	/*
+ 	  口口
+	  口口    */
+	{0x0c,0xc0, orange, 4},
+
+	/*
+	  口口口
+	  口     */
+	{0x0e,0x80, red, 6},
+
+	/*口
+	  口
+	  口口   */
+	{0x88,0xc0, red, 7},
+
+	/*
+	          口
+	  口口口   */
+	{0x01,0x70, red, 8},
+
+	/*
+	  口口
+	    口
+	    口   */
+	{0x0c,0x44, red, 5},
+
+	/*
+	  口口口
+	          口   */
+	{0x0e,0x20, brown, 10},
+
+	/*
+	    口口
+	    口
+	    口	 */
+	{0x0e,0x88, brown, 11},
+
+	/*
+	  口
+	  口口口   */
+	{0x08,0xe0, brown, 12},
+
+	/*    口
+	        口
+	    口口   */
+	{0x44,0xc0, brown, 9},
+
+	/*
+    口口口
+        口  */
+	{0x0e,0x40, green, 14},
+
+	/*
+	      口
+	      口口
+	      口   */
+	{0x04,0x64, green, 15},
+
+	/*
+           口
+	   口口口
+	       */
+	{0x04,0xe0, green, 16},
+
+	/*
+            口
+	    口口
+	        口   */
+	{0x02,0x32, green, 13},
+
+ /*    口
+	     口
+	     口
+         口  */
+	{0x44,0x44, purple, 18},
+
+	/*
+
+	  口口口口
+               */
+	{0x00,0xf0, purple, 17},
 
 
 
+};
 
 //画界面边线和背景
 void draw_windows(void);
@@ -57,10 +182,17 @@ void draw_board(void);
 //画预览区
 void draw_preview(void);
 
-//带边界的矩形
-void square_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width, color board[], color inside[]);
+//带边界的矩形 squar_flag表示是否是正方形，因为该矩形采用GLfloat定义会随着窗口尺寸变化，
+void rect_with_board(GLfloat x, GLfloat y, GLfloat f_size, GLfloat width,int squar_flag, color board[], color inside[]);
 
 //画小方块 保证在4：3的窗口下是正方形
-void draw_box(GLfloat x, GLfloat y);
+void draw_box(GLfloat x, GLfloat y, color b_color[]);
 
+//产生随机shape 返回值为 shapes数组中的索引号
+int rand_shape(void);
+
+//在预览区绘制shape
+void draw_pre_shape(int shape_num);
+
+//画方块shape
 #endif
